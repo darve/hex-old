a23a9b352d80db477b94602429f567020b02a8a4
David Woollard <dave@darve.co.uk>
2015-10-12 16:19:29 +0100

    adding some tests for the vector library

--- tests/vec.js
+++ tests/vec.js
@@ -13,63 +13,80 @@ var test    = require('tape'),
 test('Vector addition', function(t){
 
     var // Run each set of tests three times
-        count = 2;
+        count = 3;
 
     // Two tests per set
     t.plan(count * 2);
 
     // Manually add the vector components then check if the library is producting the correct result
+    // Floating point
     for ( var i = 1; i <= count; i++) {
         vecA = new Vec(M.rand(3, 2), M.rand(3, 2));
         vecB = new Vec(M.rand(3, 2), M.rand(3, 2));
+        t.deepEqual(vecA.plusNew(vecB), { x: (vecA.x + vecB.x), y: (vecA.y + vecB.y) }, vecA + ' + ' + vecB + ' === ' + vecA.plusNew(vecB));
+    }
 
-        t.deepEqual(
-            vecA.plusNew(vecB),
-            {
-                x: (vecA.x + vecB.x),
-                y: (vecA.y + vecB.y)
-            },
-            'Floating point: ' + vecA + ' + ' + vecB + ' === ' + vecA.plusNew(vecB)
-        );
+    // Integer
+    for ( var i = 1; i <= count; i++) {
+        vecA = new Vec(M.rand(10, 0), M.rand(10, 0));
+        vecB = new Vec(M.rand(10, 0), M.rand(10, 0));
+        t.deepEqual(vecA.plusNew(vecB), { x: (vecA.x + vecB.x), y: (vecA.y + vecB.y) }, vecA + ' + ' + vecB + ' === ' + vecA.plusNew(vecB));
+    }
+});
+
+test('Vector multiplication', function(t){
+
+    var // Run each set of tests three times
+        count = 3;
+
+    // Two tests per set
+    t.plan(count * 2);
+
+    for ( var i = 1; i <= count; i++) {
+        vecA = new Vec(M.rand(3, 2), M.rand(3, 2));
+        rand = M.rand(3, 2);
+        t.deepEqual(vecA.multiplyNew(rand), { x: (vecA.x * rand), y: (vecA.y * rand) }, vecA + ' * ' + rand + ' === ' + vecA.multiplyNew(rand));
     }
 
     for ( var i = 1; i <= count; i++) {
         vecA = new Vec(M.rand(10, 0), M.rand(10, 0));
-        vecB = new Vec(M.rand(10, 0), M.rand(10, 0));
-        t.deepEqual(vecA.plusNew(vecB), { x: (vecA.x + vecB.x), y: (vecA.y + vecB.y) }, 'Integer: ' + vecA + ' + ' + vecB + ' === ' + vecA.plusNew(vecB));
+        rand = M.rand(10, 0);
+        t.deepEqual(vecA.multiplyNew(rand), { x: (vecA.x * rand), y: (vecA.y * rand) }, vecA + ' * ' + rand + ' === ' + vecA.multiplyNew(rand));
     }
 });
 
-// test('Vector multiplication', function(t){
+test('Vector division', function(t){
 
-//     var // Run each set of tests three times
-//         count = 2;
+    var // Run each set of tests three times
+        count = 3;
 
-//     // Two tests per set
-//     t.plan(count * 4);
+    // Two tests per set
+    t.plan(count * 2);
 
-//     for ( var i = 1; i <= count; i++) {
-//         vecA = new Vec(M.rand(3, 2), M.rand(3, 2));
-//         rand = M.rand(3, 2);
-//         result = vecA.multiplyNew(rand);
+    for ( var i = 1; i <= count; i++) {
+        vecA = new Vec(M.rand(3, 2), M.rand(3, 2));
+        rand = M.rand(3, 2);
+        t.deepEqual(vecA.divideNew(rand), { x: (vecA.x / rand), y: (vecA.y / rand) }, vecA + ' / ' + rand + ' === ' + vecA.divideNew(rand));
+    }
 
-//         t.equal(result.x, result.x, 'Floating point: ' + vecA.x + ' * ' + rand + ' === ' + result.x);
-//         t.equal(result.y, result.y, 'Floating point: ' + vecA.y + ' * ' + rand + ' === ' + result.y);
-//     }
+    for ( var i = 1; i <= count; i++) {
+        vecA = new Vec(M.rand(10, 0), M.rand(10, 0));
+        rand = M.rand(10, 0);
+        t.deepEqual(vecA.divideNew(rand), { x: (vecA.x / rand), y: (vecA.y / rand) }, vecA + ' / ' + rand + ' === ' + vecA.divideNew(rand));
+    }
+});
 
-//     for ( var i = 1; i <= count; i++) {
-//         vecA = new Vec(M.rand(10, 0), M.rand(10, 0)),
-//         rand = M.rand(10, 0);
-//         result = vecA.multiplyNew(rand);
+test('Vector normalisation', function(t) {
 
-//         t.equal(result.x, result.x, 'Integer: ' + vecA.x + ' * ' + rand + ' === ' + result.x);
-//         t.equal(result.y, result.y, 'Integer: ' + vecA.y + ' * ' + rand + ' === ' + result.y);
-//     }
-// });
+    var // Run each set of tests three times
+        count = 3;
 
-// test('Vector normalisation', function(t) {
-//     vecA = new Vec(M.rand(3, 2), M.rand(3, 2));
-//     vecB = new Vec(M.rand(3, 2), M.rand(3, 2));
+    t.plan(count * 2);
 
-//     t.
-// });
+    for ( var i = 1; i <= count; i++) {
+        vecA = new Vec(M.rand(15, 2), M.rand(15, 2));
+        t.ok(vecA.normalise().x <= 1, 'x value of normalised vector is less than 1: ' + vecA.x);
+        t.ok(vecA.normalise().y <= 1, 'y value of normalised vector is less than 1: ' + vecA.y);
+    }
+});
+
